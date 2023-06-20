e08aaec9ad15fb29cdae0eb16d0d0cea3e8f1926
Arty Violet <myartysoul@gmail.com>
2023-06-20 18:05:32 +0300

    Changed the mentor

--- js/functions.js
+++ js/functions.js
@@ -22,15 +22,15 @@ const extractDigits = (input) => {
   return parseInt(digits.join(''), 10);
 };
 
-// console.log(checkStringLength("проверяемая строка", 20)); // true
-// console.log(checkStringLength("проверяемая строка", 10)); // false
+// console.log(checkStringLength('проверяемая строка', 20)); // true
+// console.log(checkStringLength('проверяемая строка', 10)); // false
 
 // console.log(isPalindrome('топот')); // true
 // console.log(isPalindrome('ДовОд')); // true
 // console.log(isPalindrome('Кекс')); // false
-// console.log(isPalindrome("Лёша на полке клопа нашёл ")); // true
+// console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
 
-// console.log(extractDigits("abc123xyz")); // 123
+// console.log(extractDigits('abc123xyz')); // 123
 // console.log(extractDigits('2023 год')); // 2023
 // console.log(extractDigits('ECMAScript 2022')); // 2022
 // console.log(extractDigits('1 кефир, 0.5 батона')); // 105
