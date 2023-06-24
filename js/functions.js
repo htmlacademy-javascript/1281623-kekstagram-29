ce0a6ecdf261c9d8fa7ed35e3e3db89ff434f155
Arty Violet <myartysoul@gmail.com>
2023-06-24 05:35:46 +0300

    Wrote a function isMeetingWithinWorkingHours

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,24 @@ const extractDigits = (input) => {
 
   return parseInt(digits.join(''), 10);
 };
+
+const isMeetingWithinWorkingHours = (startTime, endTime, meetingStart, meetingDuration) => {
+  const [startHours, startMinutes] = startTime.split(':').map(Number);
+  const startInMinutes = startHours * 60 + startMinutes;
+
+  const [endHours, endMinutes] = endTime.split(':').map(Number);
+  const endInMinutes = endHours * 60 + endMinutes;
+
+  const [meetingStartHours, meetingStartMinutes] = meetingStart.split(':').map(Number);
+  const meetingStartInMinutes = meetingStartHours * 60 + meetingStartMinutes;
+
+  const meetingEndInMinutes = meetingStartInMinutes + meetingDuration;
+
+  return meetingStartInMinutes >= startInMinutes && meetingEndInMinutes <= endInMinutes;
+};
+
+// console.log(isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90)); // true
+// console.log(isMeetingWithinWorkingHours('8:0', '10:0', '8:0', 120));     // true
+// console.log(isMeetingWithinWorkingHours('08:00', '14:30', '14:00', 90)); // false
+// console.log(isMeetingWithinWorkingHours('14:00', '17:30', '08:0', 90));  // false
+// console.log(isMeetingWithinWorkingHours('8:00', '17:30', '08:00', 900)); // false
