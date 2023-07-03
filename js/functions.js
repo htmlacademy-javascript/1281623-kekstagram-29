e55767b46f75a0c6083c5d9d2cf6884e0affc905
Arty Violet <myartysoul@gmail.com>
2023-07-03 06:10:31 +0300

    Added function for rendering pictures

--- js/functions.js
+++ js/functions.js
@@ -38,9 +38,3 @@ const isMeetingWithinWorkingHours = (startTime, endTime, meetingStart, meetingDu
 
   return meetingStartInMinutes >= startInMinutes && meetingEndInMinutes <= endInMinutes;
 };
-
-// console.log(isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90)); // true
-// console.log(isMeetingWithinWorkingHours('8:0', '10:0', '8:0', 120));     // true
-// console.log(isMeetingWithinWorkingHours('08:00', '14:30', '14:00', 90)); // false
-// console.log(isMeetingWithinWorkingHours('14:00', '17:30', '08:0', 90));  // false
-// console.log(isMeetingWithinWorkingHours('8:00', '17:30', '08:00', 900)); // false
