592b077dc0e69f91555358402a90bea18c5b0e0a
Arty Violet <myartysoul@gmail.com>
2023-07-06 12:05:56 +0300

    Added the function to load comments

--- js/open-picture.js
+++ js/open-picture.js
@@ -10,17 +10,47 @@ const commentsCount = document.querySelector('.comments-count');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 const localPath = 'http://localhost:3000';
+let commentsTotalCount = commentsCount.textContent;
+
+const onCommentsLoaderClick = (comments) => {
+  let counter = 5;
+
+  commentsLoader.addEventListener('click', () => {
+    counter += 5;
+
+    if (counter >= commentsTotalCount) {
+      counter = commentsTotalCount;
+      commentsLoader.classList.add('hidden');
+    }
+    socialCommentCount.textContent = `${counter} out of ${commentsTotalCount} comments`;
+    renderComments(comments.slice(0, counter));
+  });
+};
 
 const openPicture = (evt) => {
   bigPictureImg.src = evt.target.src.replace(localPath, '');
   socialCaption.textContent = evt.target.alt;
   likesCount.textContent = evt.target.nextElementSibling.querySelector('.picture__likes').textContent;
-  commentsCount.textContent = evt.target.nextElementSibling.querySelector('.picture__comments').textContent;
+  commentsTotalCount = evt.target.nextElementSibling.querySelector('.picture__comments').textContent;
 
-  renderComments(createComments(commentsCount.textContent));
+  const comments = createComments(commentsTotalCount);
+  renderComments(comments.slice(0, 5));
 
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  if (commentsTotalCount === '0') {
+    socialCommentCount.textContent = '';
+    commentsLoader.classList.add('hidden');
+  } else if (commentsTotalCount === '1') {
+    socialCommentCount.textContent = `${commentsTotalCount} comment`;
+    commentsLoader.classList.add('hidden');
+  } else if (commentsTotalCount <= 5) {
+    socialCommentCount.textContent = `${commentsTotalCount} comments`;
+    commentsLoader.classList.add('hidden');
+  } else {
+    socialCommentCount.textContent = `5 out of ${commentsTotalCount} comments`;
+    commentsLoader.classList.remove('hidden');
+  }
+
+  onCommentsLoaderClick(comments);
 
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
